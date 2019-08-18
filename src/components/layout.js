94f58684c818bc1229202c8040bbe86f7cd02237
asucarlos <kuwahara.mee@gmail.com>
2019-08-18 17:52:01 -0400

    edit about page

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import About from "./about"
 import "../styles/layout.scss"
 
 const Layout = ({ children }) => {
@@ -26,6 +27,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
+      <About />
       <div
         style={{
           margin: `0 auto`,
