378eb167d2f12dcf1d978b2b714c203892413b63
asucarlos <kuwahara.mee@gmail.com>
2019-08-20 14:55:45 -0400

    edit about page

--- src/components/about.js
+++ src/components/about.js
@@ -5,19 +5,22 @@ import { useStaticQuery, graphql } from "gatsby"
 //import components and scss
 import Image from "./image"
 import "../styles/layout.scss"
-import "../styles/about.module.scss"
+import style from "../styles/about.module.scss"
 
 
 const About = ({ children }) => {
   return (
-    <div>
-      <h1>About</h1>
-      <div style={{ maxWidth: `500px`, marginBottom: `1.45rem` }}>
-        <Image />
+    <div className={style.aboutContainer}>
+      <h1 className={style.aboutHeader}>About</h1>
+        <div className={style.aboutContentContainer}>
+          <div className={style.aboutImgContainer}>
+            <Image />
+          </div>
+          <div className={style.aboutDescription}>
+            <p>Hi, I'm Asuka Kuwahara. I'm a web developer based in Toronto. I love writing clean code and... Having recently finished my full-stack development program and Lighthouse Labs, I am currently available for full-time employment.</p>
+            <button>Download Resume</button>
+          </div>
       </div>
-      <p>Hi, I'm Asuka Kuwahara.</p>
-      <p>I'm a web developer based in Toronto. I love writing clean code and </p>
-      <p>Having recently finished my full-stack development program and Lighthouse Labs, I am currently available for full-time employment.</p>
     </div>
   )
 }
