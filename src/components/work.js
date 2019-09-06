7ba3ef6b8adab0a6f8afab31c57c2a3a163d4bdb
asucarlos <kuwahara.mee@gmail.com>
2019-09-06 11:46:50 -0400

    add jungle to work

--- src/components/work.js
+++ src/components/work.js
@@ -33,38 +33,39 @@ const Work = ({ children }) => {
       <h1 className={style.workHeader}>Work</h1>
       <div className={style.eachWorkContainer}>
         <div className={style.workImgContainer}>
-          <Img fluid={data.allImageSharp.edges[4].node.fluid} />
+          <Img fluid={data.allImageSharp.edges[5].node.fluid} />
         </div>
         <div className={style.workDescContainer}>
           <h2 className={style.workTitle}>Bidly</h2>
           <p className={style.workTechstack}>Express Node PostgreSQL React</p>
           <p className={style.workDescription}>Bidly is a full-stack strategy game based auctioning app that allows users to buy and sell items</p>
+          <p className={style.workDescription}>Login with email: anna@gmail.com password: pass123 to see full functions</p>
           <a href="https://bidlyy.herokuapp.com/" target="_blank"><FontAwesomeIcon icon={faEye} />View live</a>
           <a href="https://github.com/asucarlos/bidly" target="_blank"><FontAwesomeIcon icon={faGithubSquare} />See code</a>
         </div>
         </div>
         <div className={style.eachWorkContainer}>
         <div className={style.workImgContainer}>
-          <Img fluid={data.allImageSharp.edges[3].node.fluid} />
+          <Img fluid={data.allImageSharp.edges[4].node.fluid} />
         </div>
         <div className={style.workDescContainer}>
           <h2 className={style.workTitle}>Tweeter</h2>
           <p className={style.workTechstack}>Express Node MongoDB EJS</p>
           <p className={style.workDescription}>Tweeter is a full-stack twitter clone app which allows users to tweet</p>
-          <a href="https://bidlyy.herokuapp.com/" target="_blank"><FontAwesomeIcon icon={faEye} />View live</a>
-          <a href="https://github.com/asucarlos/bidly" target="_blank"><FontAwesomeIcon icon={faGithubSquare} />See code</a>
+          <a href="https://tweeter-aska.herokuapp.com/" target="_blank"><FontAwesomeIcon icon={faEye} />View live</a>
+          <a href="https://github.com/asucarlos/tweeter" target="_blank"><FontAwesomeIcon icon={faGithubSquare} />See code</a>
         </div>
         </div>
         <div className={style.eachWorkContainer}>
         <div className={style.workImgContainer}>
-          {/* <Img fluid={data.placeholderImage.childImageSharp.fluid}/>       */}
+        <Img fluid={data.allImageSharp.edges[2].node.fluid} />
         </div>
         <div className={style.workDescContainer}>
           <h2 className={style.workTitle}>Jungle</h2>
-          <p className={style.workTechstack}>Express Node PostgreSQL React</p>
-          <p className={style.workDescription}>Bidly is a full-stack strategy game based auctioning app that allows users to buy and sell items</p>
-          <a href="https://tweeter-aska.herokuapp.com/" target="_blank"><FontAwesomeIcon icon={faEye} />View live</a>
-          <a href="https://github.com/asucarlos/tweeter" target="_blank"><FontAwesomeIcon icon={faGithubSquare} />See code</a>
+          <p className={style.workTechstack}>Ruby on Rails PostgreSQL Stripe</p>
+          <p className={style.workDescription}>Jungle is a mini e-commerce application built with Ruby on Rails</p>
+          {/* <a href="https://tweeter-aska.herokuapp.com/" target="_blank"><FontAwesomeIcon icon={faEye} />View live</a> */}
+          <a href="https://github.com/asucarlos/jungle-rails" target="_blank"><FontAwesomeIcon icon={faGithubSquare} />See code</a>
         </div>
         </div>
       </div>
