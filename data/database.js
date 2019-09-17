a7abd42ba8fe79a649aff9970f3f51ed03abe8b8
asucarlos <kuwahara.mee@gmail.com>
2019-09-17 17:52:02 -0400

    change images/data structure

---
+++ data/database.js
@@ -0,0 +1,35 @@
+const workData = [
+  {
+    1: {
+      name: "Bidly",
+      tech_stack: "Express Node PostgreSQL React",
+      image: "",
+      description:
+        "strategy game based auctioning app that allows users to buy and sell items Login with email: anna@gmail.com password: pass123 to see full functions.",
+      live: "https://bidlyy.herokuapp.com/",
+      github: "https://github.com/asucarlos/bidly",
+    },
+  },
+  {
+    2: {
+      name: "Tweeter",
+      tech_stack: "Express Node MongoDB EJS",
+      image: "",
+      description:
+        "Tweeter is a full-stack twitter clone app which allows users to tweet",
+      live: "https://tweeter-aska.herokuapp.com/",
+      github: "https://github.com/asucarlos/tweeter",
+    },
+  },
+  {
+    3: {
+      name: "Jungle",
+      tech_stack: "Ruby on Rails PostgreSQL Stripe",
+      image: "",
+      description:
+        "Jungle is a mini e-commerce application built with Ruby on Rails",
+      live: "",
+      github: "https://github.com/asucarlos/jungle-rails",
+    },
+  },
+]
