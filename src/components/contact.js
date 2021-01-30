ad0d3594aaceb37d63620433d9263c719a16f5b1
asucarlos <kuwahara.mee@gmail.com>
2021-01-29 23:21:21 -0500

    replace Instagram with dev.to icon

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,25 +1,19 @@
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   faGithubSquare,
   faLinkedin,
-  faInstagram,
+  faDev,
 } from "@fortawesome/free-brands-svg-icons"
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
-
-//import components and scss
-import "../styles/layout.scss"
 import style from "../styles/contact.module.scss"
 
-const Contact = ({ children }) => {
+const Contact = () => {
   const contactInfo = {
     email: "kuwahara.mee@gmail.com",
     linkedin: "https://www.linkedin.com/in/asuka-kuwahara/",
     github: "https://github.com/asucarlos",
-    instagram: "https://www.instagram.com/askarlosnaps/",
+    devTo: "https://dev.to/asucarlos",
   }
 
   return (
@@ -27,27 +21,31 @@ const Contact = ({ children }) => {
       <h1>Contact me</h1>
       <div className={style.iconsContainer}>
         <div className={style.iconContainer}>
-          <a href={"mailto:" + `${contactInfo.email}`}>
+          <a
+            href={`mailto: ${contactInfo.email}`}
+            target="_blank"
+            rel="noreferrer"
+          >
             <FontAwesomeIcon icon={faEnvelope} />
             <p>{contactInfo.email}</p>
           </a>
         </div>
         <div className={style.iconContainer}>
-          <a href={contactInfo.linkedin} target="_blank">
+          <a href={contactInfo.linkedin} target="_blank" rel="noreferrer">
             <FontAwesomeIcon icon={faLinkedin} />
             <p>@asuka-kuwahara</p>
           </a>
         </div>
         <div className={style.iconContainer}>
-          <a href={contactInfo.github} target="_blank">
+          <a href={contactInfo.github} target="_blank" rel="noreferrer">
             <FontAwesomeIcon icon={faGithubSquare} />
             <p>@asucarlos</p>
           </a>
         </div>
         <div className={style.iconContainer}>
-          <a href={contactInfo.instagram} target="_blank">
-            <FontAwesomeIcon icon={faInstagram} />
-            <p>@askarlosnaps</p>
+          <a href={contactInfo.devTo} target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faDev} />
+            <p>@asucarlos</p>
           </a>
         </div>
       </div>
